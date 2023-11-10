3fce8a15b8163cf9febc332ec954763849a9c054
Salma Maarouf <68617759+SalmaW@users.noreply.github.com>
2023-11-10 23:08:52 +0200

    updating the order and finishing i am poor challenge

---
+++ Course practices/i_am_poor_challenge/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.lightGreen[200],
+          appBar: AppBar(
+            title: Text('I Am Poor'),
+            backgroundColor: Colors.green,
+          ),
+          body: Center(
+            child: Image(
+              image: AssetImage('assets/images/poor.png'),
+            ),
+          ),
+        ),
+      ),
+    );
